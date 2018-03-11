f9db432ad50533a797b18afef179fcee015eb65b
Drewol <emildraws@gmail.com>
2018-03-11 02:06:48 +0100

    Improved timezone dictionary building.

--- pytz_dict.py
+++ pytz_dict.py
@@ -5,7 +5,14 @@ def get_dict():
 	fmt = "%Z"
 	ret = {}
 	for tz in pytz.all_timezones:
-		dat = datetime.utcnow()
+		#Winter
+		dat = datetime(2000,1,1)
+		dat = dat.replace(tzinfo=pytz.utc)
+		dat = dat.astimezone(pytz.timezone(tz))
+		ret[dat.strftime(fmt)] = pytz.timezone(tz)
+		
+		#Summer
+		dat = datetime(2000,6,16)
 		dat = dat.replace(tzinfo=pytz.utc)
 		dat = dat.astimezone(pytz.timezone(tz))
 		ret[dat.strftime(fmt)] = pytz.timezone(tz)
